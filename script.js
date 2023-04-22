fa9f6f1b6c42bca22fba23b7a8ca038c9becf6c8
jmkenny94 <jmkenny94@gmail.com>
2023-04-22 11:07:00 -0400

    redo

---
+++ script.js
@@ -0,0 +1,56 @@
+// portfolio slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// preloader
+window.addEventListener("load", function () {
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("hide-preloader");
+});
+
+// responsive navigation
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("show");
+});
